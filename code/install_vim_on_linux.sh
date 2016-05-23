b9fedfbf2d3235c62580ad36c4e11f2eed6f7b70
gfcocos <gfcocos@gmail.com>
2016-05-23 13:50:27 +0800

    del old project

--- code/install_vim_on_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git clone -b vimrc https://git.oschina.net/gfcocos/ubuntu.git ~/.vim_runtime
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
-ln -s $HOME/.vim_runtime/.vimrc $HOME/.vimrc
-
